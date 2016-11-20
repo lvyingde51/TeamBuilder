32f0987ed855aacfc7ad5df152526ce5b651319b
Seung Jae (Ryan) Lee <sjlee235@gmail.com>
2016-11-20 06:32:22 -0500

    Add static page

--- app.js
+++ app.js
@@ -10,11 +10,13 @@ var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log("%s listening to %s", server.name, server.url);
 });
+server.get(/.*/, restify.serveStatic({
+	'directory': '.',
+	'default': 'index.html'
+}));
 
 // Create Chat Bot
 var connector = new builder.ChatConnector({
-//    appId: process.env.MICROSOFT_APP_ID,
-//    appPassword: process.env.MICROSOFT_APP_PASSWORD
     appId: 'dbe6cde4-afd9-4e59-9b27-3a5a027b9210',
     appPassword: 'Y2UA7SSOkwtvtna1OWKhmoz'
 });
