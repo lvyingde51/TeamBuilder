e9a6f6cf0a969c368e8423132bc9089a8bc4f450
Seung Jae (Ryan) Lee <sjlee235@gmail.com>
2016-11-20 06:45:30 -0500

    Delete ID/Password from
    Azure app settings should be enough for it

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ server.get(/.*/, restify.serveStatic({
 
 // Create Chat Bot
 var connector = new builder.ChatConnector({
-    appId: 'dbe6cde4-afd9-4e59-9b27-3a5a027b9210',
-    appPassword: 'Y2UA7SSOkwtvtna1OWKhmoz'
+    appId: process.env.appId,
+    appPassword: process.env.appPassword
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
