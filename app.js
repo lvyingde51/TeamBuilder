0fe19c07c7b7eb8867273a2d01fb442d4bc732e8
Seung Jae (Ryan) Lee <sjlee235@gmail.com>
2016-11-20 06:13:44 -0500

    Change App ID and Password

--- app.js
+++ app.js
@@ -13,8 +13,10 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create Chat Bot
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+//    appId: process.env.MICROSOFT_APP_ID,
+//    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: 'dbe6cde4-afd9-4e59-9b27-3a5a027b9210',
+    appPassword: 'Y2UA7SSOkwtvtna1OWKhmoz'
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
